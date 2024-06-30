a329114e3dc843657992b4f7d005a05c2f054533
kamleshgupta12 <bittugupta2743@gmail.com>
2024-06-30 19:31:25 +0530

    static API

---
+++ Basic API Create/data.js
@@ -0,0 +1,10 @@
+const data =[
+    {name:"test",email:"test@gmail.com"},
+    {name:"ram",email:"ram@gmail.com"},
+    {name:"sit",email:"sita@gmail.com"},
+    {name:"raj",email:"raj@gmail.com"},
+    {name:"rani",email:"rani@gmail.com"}
+]
+
+
+module.exports=data;
